53782d17086d0d9cb2c46bd5522d994511f33dc9
JHFarrant <JHFarrant@live.co.uk>
2015-03-10 17:34:03 +0000

    worked out what to do with matrix exponent
    Former-commit-id: eea72c8197f883e62431a90cdae0ce91e7f15df0

--- provider/user_matrix_exponent.hpp
+++ provider/user_matrix_exponent.hpp
@@ -24,18 +24,44 @@ public:
 
       log->LogVerbose("Beginning multiplication");
       hash[0]=acc[0];
+      //OpenCL Here (Include hash vector inside MatrixMul Kernel)
       for(unsigned i=1; i<input->steps; i++){
         log->LogDebug("Iteration %d", i);
         acc=MatrixMul(input->n,acc, A);
         hash[i]=acc[0];
       }
       log->LogVerbose("Done");
-
+	
       output->hashes=hash;
 
     return;
   }
-
+  
+protected:
+    static std::vector<uint32_t> MatrixIdentity(unsigned n)
+    {
+    //Run open_cl Implementation here.
+      std::vector<uint32_t> res(n*n,0);
+      for(unsigned i=0; i<n; i++){
+        res[i*n+i]=1;
+      }
+      return res;
+    }
+    
+    static std::vector<uint32_t> MatrixMul(unsigned n, std::vector<uint32_t> a, std::vector<uint32_t> b)
+    {
+      std::vector<uint32_t> res(n*n, 0);
+      //Run open_cl Implementation here.
+      for(unsigned r=0; r<n; r++){
+        for(unsigned c=0; c<n; c++){
+          for(unsigned i=0; i<n; i++){
+            res[r*n+c] = Add(res[r*n+c], Mul(a[r*n+i], b[i*n+r]));
+          }
+        }
+      }
+      return res;
+    }
+    
 };
 
 #endif
