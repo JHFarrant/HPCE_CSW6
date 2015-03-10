98566985d9b5020e619bc59cd6fb15ab74f86686
Jack Farrant <JHFarrant@live.co.uk>
2015-03-10 14:43:33 +0000

    Started to identify areas for improvement

--- provider/user_matrix_exponent.hpp
+++ provider/user_matrix_exponent.hpp
@@ -15,7 +15,25 @@ public:
 		       const puzzler::MatrixExponentInput *input,
 		       puzzler::MatrixExponentOutput *output
 		       ) const override {
-    return ReferenceExecute(log, input, output);
+
+    std::vector<uint32_t> hash(input->steps);
+
+      log->LogVerbose("Setting up A and identity");
+      auto A=MatrixCreate(input->n, input->seed);
+      auto acc=MatrixIdentity(input->n);
+
+      log->LogVerbose("Beginning multiplication");
+      hash[0]=acc[0];
+      for(unsigned i=1; i<input->steps; i++){
+        log->LogDebug("Iteration %d", i);
+        acc=MatrixMul(input->n,acc, A);
+        hash[i]=acc[0];
+      }
+      log->LogVerbose("Done");
+
+      output->hashes=hash;
+
+    return;
   }
 
 };
