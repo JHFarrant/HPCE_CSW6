317ac3eb17e99b59d3a5d9180dd47a1451c6f15a
JHFarrant <JHFarrant@live.co.uk>
2015-03-11 11:55:40 +0000

    Implemented setup opencl function. CONTAINS ERRORS

--- provider/user_matrix_exponent.hpp
+++ provider/user_matrix_exponent.hpp
@@ -2,6 +2,21 @@
 #define user_matrix_exponent_hpp
 
 #include "puzzler/puzzles/matrix_exponent.hpp"
+struct openCLinstance {
+  cl::Kernel kernel_1;
+  cl::Kernel kernel_2;
+  cl::CommandQueue queue;
+  cl::Buffer buffer_1;
+  cl::Buffer buffer_2;
+  cl::Buffer buffer_3;
+  cl::Context context;
+  int selected_device;
+} ;
+
+
+
+
+
 
 class MatrixExponentProvider
   : public puzzler::MatrixExponentPuzzle
@@ -61,6 +76,105 @@ protected:
       }
       return res;
     }
+    
+    static std::string LoadSource(const char *fileName)
+{
+    // Don't forget to change your_login here
+    std::string baseDir="src/jf1711";
+    if(getenv("HPCE_CL_SRC_DIR")){
+        baseDir=getenv("HPCE_CL_SRC_DIR");
+    }
+
+    std::string fullName=baseDir+"/"+fileName;
+
+    // Open a read-only binary stream over the file
+    std::ifstream src(fullName, std::ios::in | std::ios::binary);
+    if(!src.is_open())
+        throw std::runtime_error("LoadSource : Couldn't load cl file from '"+fullName+"'.");
+
+    // Read all characters of the file into a string
+    return std::string(
+        (std::istreambuf_iterator<char>(src)), // Node the extra brackets.
+        std::istreambuf_iterator<char>()
+    );
+}
+static openCLinstance* setup_opencl()
+{
+	try{
+		openCLinstance opencl1;
+		
+		std::vector<cl::Platform> platforms;
+			
+		cl::Platform::get(&platforms);
+		if(platforms.size()==0)
+			throw std::runtime_error("No OpenCL platforms found.");
+		
+		std::cerr<<"Found "<<platforms.size()<<" platforms\n";
+		for(unsigned i=0;i<platforms.size();i++){
+			std::string vendor=platforms[i].getInfo<CL_PLATFORM_VENDOR>();
+			std::cerr<<"  Platform "<<i<<" : "<<vendor<<"\n";
+		}
+		
+		int selectedPlatform=0;
+		if(getenv("HPCE_SELECT_PLATFORM")){
+			selectedPlatform=atoi(getenv("HPCE_SELECT_PLATFORM"));
+		}
+		std::cerr<<"Choosing platform "<<selectedPlatform<<"\n";
+		cl::Platform platform=platforms.at(selectedPlatform);
+		
+		std::vector<cl::Device> devices;
+		platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);	
+		if(devices.size()==0){
+			throw std::runtime_error("No opencl devices found.\n");
+		}
+			
+		std::cerr<<"Found "<<devices.size()<<" devices\n";
+		for(unsigned i=0;i<devices.size();i++){
+			std::string name=devices[i].getInfo<CL_DEVICE_NAME>();
+			std::cerr<<"  Device "<<i<<" : "<<name<<"\n";
+		}
+		
+		int selectedDevice=0;
+		if(getenv("HPCE_SELECT_DEVICE")){
+			selectedDevice=atoi(getenv("HPCE_SELECT_DEVICE"));
+		}
+		std::cerr<<"Choosing device "<<selectedDevice<<"\n";
+		cl::Device device=devices.at(selectedDevice);
+		
+		opencl1.selected_device = selectedDevice;
+		
+		cl::Context context(devices);
+		
+		opencl1.context = context;
+		
+		std::string kernelSource="";
+		
+		cl::Program::Sources sources(1, std::make_pair(kernelSource.c_str(), kernelSource.size()+1));
+		
+		cl::Program program(context, sources);
+		program.build(devices);
+		
+		unsigned nThreads=1;
+		
+		size_t cbBuffer=4;
+		cl::Buffer buffer(context, CL_MEM_READ_WRITE, cbBuffer);
+		
+		cl::Kernel kernel_1(program, "Add");
+		
+		opencl1.kernel_1 = kernel_1;
+		
+		cl::CommandQueue queue(context, device);
+		
+		opencl1.queue = queue; 
+		
+	}catch(const std::exception &e){
+		std::cerr<<"Exception : "<<e.what()<<std::endl;
+		return 1;
+	}
+		
+	return opencl1;
+}
+
     
 };
 
